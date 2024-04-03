6106211006c3e50c8de6416875b16b3c795ddde2
Unmani Shinde <mailto.unmani@gmail.com>
2024-04-04 02:46:08 +0530

    frontend started

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import VotingPage from './pages/VotingPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <VotingPage/>
+    </div>
+  );
+}
+
+export default App;
